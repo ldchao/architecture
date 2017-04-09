fdd4263c2d14f3a4829e9e454edcf76db53fec94
lxl14 <lxl14@software.nju.edu.cn>
2017-04-09 23:00:44 +0800

    修改

---
+++ BestPrice/src/main/java/service/ShowGoodsInfoService.java
@@ -0,0 +1,26 @@
+package service;
+
+import java.util.List;
+import vo.GoodInfo;
+
+/**
+ * Created by Xuanlin on 2017/4/9.
+ */
+
+public interface ShowGoodsInfoService {
+
+
+
+    public List<GoodInfo> getByShopId(int shopId,List<GoodInfo> goodList);
+
+
+    public GoodInfo getByGoodId(int goodId,List<GoodInfo> goodList);
+
+
+    public GoodInfo getByGoodName(String name,List<GoodInfo> goodList);
+
+
+
+
+}
+t;
---
+++ BestPrice/src/main/java/service/serviceimpl/ShowGoodsInfoImpl.java
@@ -0,0 +1,29 @@
+package service.serviceimpl;
+
+import service.ShowGoodsInfoService;
+import java.util.List;
+import vo.GoodInfo;
+
+
+/**
+ * Created by Xuanlin on 2017/4/9.
+ */
+
+public class ShowGoodsInfoImpl implements ShowGoodsInfoService {
+
+
+
+    public List<GoodInfo> getByShopId(int shopId,List<GoodInfo> goodList) {
+        return null;
+    }
+
+    public GoodInfo getByGoodId(int goodId,List<GoodInfo> goodList) {
+        return null;
+    }
+
+    public GoodInfo getByGoodName(String name,List<GoodInfo> goodList) {
+        return null;
+    }
+
+
+}
