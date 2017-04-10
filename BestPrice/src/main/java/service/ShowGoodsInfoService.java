677fe09f6fcbba1f2bb8d116ce02deeb28edfdf4
lxl14 <lxl14@software.nju.edu.cn>
2017-04-10 23:22:23 +0800

    补注释

--- BestPrice/src/main/java/service/ShowGoodsInfoService.java
+++ BestPrice/src/main/java/service/ShowGoodsInfoService.java
@@ -9,12 +9,28 @@ import vo.GoodInfo;
 
 public interface ShowGoodsInfoService {
 
+    /**
+     * 返回列表中属于该商店的所有商品
+     * @param shopId
+     * @param goodList
+     * @return
+     */
     public List<GoodInfo> getByShopId(int shopId,List<GoodInfo> goodList);
 
-
+    /**
+     * 根据商品id返回一条商品信息
+     * @param goodId
+     * @param goodList
+     * @return
+     */
     public GoodInfo getByGoodId(int goodId,List<GoodInfo> goodList);
 
-
+    /**
+     * 根据商品名称返回一条商品信息
+     * @param name
+     * @param goodList
+     * @return
+     */
     public GoodInfo getByGoodName(String name,List<GoodInfo> goodList);
 
 
--- BestPrice/src/main/java/service/serviceimpl/ShowGoodsInfoImpl.java
+++ BestPrice/src/main/java/service/serviceimpl/ShowGoodsInfoImpl.java
@@ -1,5 +1,6 @@
 package service.serviceimpl;
 
+import org.springframework.stereotype.Service;
 import service.ShowGoodsInfoService;
 import java.util.List;
 import vo.GoodInfo;
@@ -12,16 +13,18 @@ import vo.GoodInfo;
 public class ShowGoodsInfoImpl implements ShowGoodsInfoService {
 
 
-
     public List<GoodInfo> getByShopId(int shopId,List<GoodInfo> goodList) {
+
         return null;
     }
 
     public GoodInfo getByGoodId(int goodId,List<GoodInfo> goodList) {
+
         return null;
     }
 
     public GoodInfo getByGoodName(String name,List<GoodInfo> goodList) {
+
         return null;
     }
 
