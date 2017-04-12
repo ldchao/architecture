8083232c50155a1f077fa517e2575c774fe39396
Billdd <141250032@smail.nju.edu.cn>
2017-04-13 01:04:28 +0800

    comment,product,seller的save

--- BestPrice/src/main/java/dao/daoimpl/CommentDaoImpl.java
+++ BestPrice/src/main/java/dao/daoimpl/CommentDaoImpl.java
@@ -2,6 +2,9 @@ package dao.daoimpl;
 
 import Entity.Comment;
 import dao.CommentDao;
+import dao.MainConnection;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -11,7 +14,24 @@ import java.util.List;
 public class CommentDaoImpl implements CommentDao {
 
     public void save(Comment comment) {
-        //TODO
+        Session session= MainConnection.getSession();
+        try {
+            if (fetch(comment.getId())==null){
+               // System.out.println("in!");
+                session.save(comment);
+                Transaction transaction=session.beginTransaction();
+                transaction.commit();
+                MainConnection.closeSession(session);
+
+            }else {
+                MainConnection.closeSession(session);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            MainConnection.closeSession(session);
+
+        }
     }
 
     public Comment fetch(int id) {
