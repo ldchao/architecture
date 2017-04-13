d8c8c2ca7d0069aaeae4c914031283efa68136ba
ldchao <379905230@qq.com>
2017-04-13 22:16:07 +0800

    搜索商品排序模块

--- BestPrice/src/main/java/service/search_good_sort/SortByPrice.java
+++ BestPrice/src/main/java/service/search_good_sort/SortByPrice.java
@@ -2,22 +2,19 @@ package service.search_good_sort;
 
 import vo.GoodVO;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Collections;
 
 /**
- * Created by Administrator on 2017/4/9.
+ * Created by ldchao on 2017/4/13.
  */
-public class SortByBidding implements SortStrategy{
-
+public class SortByPrice implements SortStrategy{
     public void sort(List<GoodVO> goodVOList) {
-
         Collections.sort(goodVOList, new Comparator() {
             public int compare(Object o1, Object o2) {
-                return ((GoodVO)o2).getCompetePrice().compareTo(((GoodVO)o1).getCompetePrice());
+                return ((GoodVO)o1).getPrice().compareTo(((GoodVO)o2).getPrice());
             }
         });
-
     }
 }
