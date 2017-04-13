e3d49233cc611cf5b4078878d77392e9a118bf22
lxl14 <lxl14@software.nju.edu.cn>
2017-04-14 07:37:37 +0800

    商品信息＋评论＋销售记录 view

--- BestPrice/src/main/java/controller/CommentsController.java
+++ BestPrice/src/main/java/controller/CommentsController.java
@@ -1,10 +1,36 @@
 package controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.CommentService;
+import vo.CommentVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
- * Created by Administrator on 2017/4/12.
+ * Created by Xuanlin on 2017/4/12.
  */
-@Controller
+
+
+
+@RequestMapping(value = "/comment")
 public class CommentsController {
+    @Autowired
+    CommentService commentService;
+
+    private String success = "success";
+    private String input = "input";
+
+
+    @RequestMapping(value = "/makeComment")
+    @ResponseBody
+    public String makeComment(String content,HttpServletRequest request) {
+        CommentVO commentVO =new CommentVO();
+        commentVO.setContent(content);
+//unfinished
+        return "success";
+    }
+
+
 }
