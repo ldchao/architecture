5dca4cb5dcf8cc510dc4898201c0d32f6112a16f
marioquer <marioquer@hotmail.com>
2017-04-13 20:39:23 +0800

    登录注册controller

--- BestPrice/src/main/java/controller/UserController.java
+++ BestPrice/src/main/java/controller/UserController.java
@@ -1,7 +1,54 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.UserService;
+import vo.UserVO;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Administrator on 2017/4/12.
  */
+@RequestMapping(value = "/user")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    public String login(String username, String password, HttpServletRequest request) {
+        UserVO userVO = userService.login(username, password);
+        if (userVO == null) {
+            return "fail";
+        } else {
+            request.getSession().setAttribute("user", userVO);
+            return "success";
+        }
+    }
+
+    @RequestMapping(value = "/logup")
+    @ResponseBody
+    public String logup(String username, String password, String email, String address, HttpServletRequest request) {
+        UserVO userVO = new UserVO();
+        userVO.setName(username);
+        userVO.setPassword(password);
+        userVO.setEmail(email);
+        userVO.setAddress(address);
+        UserVO result  = userService.register(userVO);
+        if (result == null) {
+            return "fail";
+        } else {
+            return "success";
+        }
+    }
+
+
+    @RequestMapping(value = "/logout")
+    @ResponseBody
+    public String logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("user");
+        return "success";
+    }
 }
--- BestPrice/src/main/webapp/pages/commonJSP/header.jsp
+++ BestPrice/src/main/webapp/pages/commonJSP/header.jsp
@@ -10,10 +10,10 @@
     <div class="nav-wrapper container">
         <a id="logo-container" href="#" class="brand-logo blue-grey-text">爱比价网</a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="javascript:logout(${sessionScope.get("user").getUserId()})" class="teal-text">登出</a></li>
+            <li><a href="javascript:logout()" class="teal-text">登出</a></li>
         </ul>
         <ul id="nav-mobile" class="side-nav">
-            <li><a href="javascript:logout(${sessionScope.get("user").getUserId()})">登出</a></li>
+            <li><a href="javascript:logout()">登出</a></li>
         </ul>
         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
