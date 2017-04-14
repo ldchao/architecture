cd3a02a57cca92855ad7531f4585aef236aeea29
ldchao <379905230@qq.com>
2017-04-14 18:04:16 +0800

    good

--- BestPrice/src/main/java/controller/GoodsController.java
+++ BestPrice/src/main/java/controller/GoodsController.java
@@ -15,6 +15,7 @@ import vo.*;
 import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 
@@ -33,75 +34,70 @@ public class GoodsController{
     PurchaseService purchaseService;
 
     @RequestMapping(value ="/showGoodsInfo")
-    public ModelAndView showGoodsInfo(int GoodId, HttpServletRequest request) {
-//        ServletContext sc = request.getServletContext();
-        ModelAndView modelAndView=new ModelAndView();
+    @ResponseBody
+    public String showGoodsInfo(int GoodId, HttpServletRequest request) {
+        HttpSession sc = request.getSession();
         GoodVO goodVO = showGoodsService.getByGoodId(GoodId);
-        modelAndView.addObject("GoodVO",goodVO);
-//        String goodid=Integer.toString(GoodId);
-//        String price=Double.toString(goodVO.getPrice());
-//        String description=goodVO.getDescription();
-//        String product_name=goodVO.getProduct_name();
-//        String seller_name=goodVO.getSeller_name();
-//        String platform=goodVO.getPlatform();
-//        String isJoin=pickIsJoin(goodVO.getIsJoin());
-//
-//        sc.setAttribute("goodid", goodid);
-//        sc.setAttribute("price",price);
-//        sc.setAttribute("description",description);
-//        sc.setAttribute("product_name",product_name);
-//        sc.setAttribute("seller_name",seller_name);
-//        sc.setAttribute("platform",platform);
-//        sc.setAttribute("isJoin",isJoin);
-//        sc.setAttribute("sales_volume",goodVO.getSales_volume());
-//        sc.setAttribute("comments_volume",goodVO.getComments_volume());
-//
-//
-//        List<CommentVO> cl=showGoodsService.getCommentByGoodId(GoodId);
-//        List<BuyRecordVO> bl =showGoodsService.getByShopId(GoodId);
-//
-//        String[] comment_date = new String[cl.size()];
-//        String[] comment_UsersId=new String[cl.size()];
-//        String[] comment_content = new String[cl.size()];
-//
-//
-//        String[] buy_UserId = new String[bl.size()];
-//        String[] buy_date= new String[bl.size()];
-//        String[] buy_price= new String[bl.size()];
-//        String[] buy_num= new String[bl.size()];
-//
-//
-//        for(int i = 0;i<cl.size();i++){
-//            comment_date[i]=cl.get(i).getTime();
-//            comment_UsersId[i]=Integer.toString(cl.get(i).getUserid());
-//            comment_content[i]=cl.get(i).getContent();
-//
-//            System.out.println(cl.get(i).getTime());
-//            System.out.println(cl.get(i).getUserid());
-//            System.out.println(cl.get(i).getContent());
-//        }
-//
-//        for(int i = 0;i<bl.size();i++){
-//            buy_UserId[i]=Integer.toString(bl.get(i).getUserid());
-//            buy_date[i] =bl.get(i).getBuydate();
-//            buy_price[i]=Double.toString(bl.get(i).getTotalPrice());
-//            buy_num [i]=Integer.toString(bl.get(i).getBuynum());
-//        }
-//
-//        sc.setAttribute("comment_date", comment_date);
-//        sc.setAttribute("comment_UsersId", comment_UsersId);
-//        sc.setAttribute("comment_content", comment_content);
-//        sc.setAttribute("num_comment", comment_date.length);  //次数
-//        sc.setAttribute("buy_UserId", buy_UserId);
-//        sc.setAttribute("buy_date", buy_date);
-//        sc.setAttribute("buy_price", buy_price);
-//        sc.setAttribute("buy_num", buy_num);
-//        sc.setAttribute("num_buy", buy_date.length); //次数
-//
-//
-//
-//        return success;
-        return modelAndView;
+        String goodid=Integer.toString(GoodId);
+        String price=Double.toString(goodVO.getPrice());
+        String description=goodVO.getDescription();
+        String product_name=goodVO.getProduct_name();
+        String seller_name=goodVO.getSeller_name();
+        String platform=goodVO.getPlatform();
+        String isJoin=pickIsJoin(goodVO.getIsJoin());
+
+        sc.setAttribute("goodid", goodid);
+        sc.setAttribute("price",price);
+        sc.setAttribute("description",description);
+        sc.setAttribute("product_name",product_name);
+        sc.setAttribute("seller_name",seller_name);
+        sc.setAttribute("platform",platform);
+        sc.setAttribute("isJoin",isJoin);
+        sc.setAttribute("sales_volume",goodVO.getSales_volume());
+        sc.setAttribute("comments_volume",goodVO.getComments_volume());
+
+
+        List<CommentVO> cl=showGoodsService.getCommentByGoodId(GoodId);
+        List<BuyRecordVO> bl =showGoodsService.getByShopId(GoodId);
+
+        String[] comment_date = new String[cl.size()];
+        String[] comment_UsersId=new String[cl.size()];
+        String[] comment_content = new String[cl.size()];
+
+
+        String[] buy_UserId = new String[bl.size()];
+        String[] buy_date= new String[bl.size()];
+        String[] buy_price= new String[bl.size()];
+        String[] buy_num= new String[bl.size()];
+
+
+        for(int i = 0;i<cl.size();i++){
+            comment_date[i]=cl.get(i).getTime();
+            comment_UsersId[i]=Integer.toString(cl.get(i).getUserid());
+            comment_content[i]=cl.get(i).getContent();
+
+            System.out.println(cl.get(i).getTime());
+            System.out.println(cl.get(i).getUserid());
+            System.out.println(cl.get(i).getContent());
+        }
+
+        for(int i = 0;i<bl.size();i++){
+            buy_UserId[i]=Integer.toString(bl.get(i).getUserid());
+            buy_date[i] =bl.get(i).getBuydate();
+            buy_price[i]=Double.toString(bl.get(i).getTotalPrice());
+            buy_num [i]=Integer.toString(bl.get(i).getBuynum());
+        }
+
+        sc.setAttribute("comment_date", comment_date);
+        sc.setAttribute("comment_UsersId", comment_UsersId);
+        sc.setAttribute("comment_content", comment_content);
+        sc.setAttribute("num_comment", comment_date.length);  //次数
+        sc.setAttribute("buy_UserId", buy_UserId);
+        sc.setAttribute("buy_date", buy_date);
+        sc.setAttribute("buy_price", buy_price);
+        sc.setAttribute("buy_num", buy_num);
+        sc.setAttribute("num_buy", buy_date.length); //次数
+        return success;
 
     }
 
